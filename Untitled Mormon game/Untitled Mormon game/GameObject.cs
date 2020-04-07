54961fcb8f9351f14b5b250aa8b7421fc68ff406
Mads Oddershede Geertsen <mads40g4@student.eadania.dk>
2020-04-07 11:46:10 +0200

    Bla Bla

---
+++ Untitled Mormon game/Untitled Mormon game/GameObject.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untitled_Mormon_game
+{
+    abstract class GameObject
+    {
+        Texture2D texture;
+        Texture2D[] textures;
+        Vector2 position;
+        float speed;
+        Vector2 velocity;
+        int fps;
+        int currentIndex;
+
+
+        public abstract void LoadContent(ContentManager content);
+
+        public abstract void Update(GameTime gameTime);
+
+        protected void Animation(GameTime gameTime)
+        {
+
+        }
+
+
+    }
+}
---
+++ Untitled Mormon game/Untitled Mormon game/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace Untitled_Mormon_game
+{
+    class Player : GameObject
+    {
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
