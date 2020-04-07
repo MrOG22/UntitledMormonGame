d0700c3a78bd294bb442a4477af4c1000c341bf5
Mads Oddershede Geertsen <mads40g4@student.eadania.dk>
2020-04-07 12:06:48 +0200

    NEW UPDATE

---
+++ Untitled Mormon game/Untitled Mormon game/Follower.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Untitled_Mormon_game
+{
+    class Follower : GameObject
+    {
+        private Texture2D followerTex;
+
+        public override void LoadContent(ContentManager content)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Follow()
+        {
+
+        }
+    }
+}
