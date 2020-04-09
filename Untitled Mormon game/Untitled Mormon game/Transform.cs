4354db99f8034fe269b08b74cbe6adccca76fd65
Mads Oddershede Geertsen <mads40g4@student.eadania.dk>
2020-04-09 12:41:01 +0200

    Transform

---
+++ Untitled Mormon game/Untitled Mormon game/Transform.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untitled_Mormon_game
+{
+    class Transform
+    {
+        /// <summary>
+        /// The transform's position
+        /// </summary>
+        public Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Changes the position based on a translation
+        /// </summary>
+        /// <param name="translation"></param>
+        public void Translate(Vector2 translation)
+        {
+            if (!float.IsNaN(translation.X) && !float.IsNaN(translation.Y))
+            {
+                Position += translation;
+            }
+
+        }
+    }
+}
