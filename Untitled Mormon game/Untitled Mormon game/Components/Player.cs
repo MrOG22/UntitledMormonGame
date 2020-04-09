ff728f61774bf5dbd0bf1cfcd196d3f71329f634
Mads Oddershede Geertsen <mads40g4@student.eadania.dk>
2020-04-09 14:35:38 +0200

    NYT NYT NYT NYT SHIT

--- Untitled Mormon game/Untitled Mormon game/Components/Player.cs
+++ Untitled Mormon game/Untitled Mormon game/Components/Player.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,46 @@ namespace Untitled_Mormon_game.Components
 {
     class Player : Component
     {
+        public float rotationVelocity = 3f;
+        public float linearVelocity = 4f;
+        private Vector2 direction;
+        private Transform transform;
+        private float speed;
+        private Vector2 position;
+
+        public Player(Vector2 startPosition)
+        {
+            transform = new Transform();
+
+            transform.Position = startPosition;
+
+            this.speed = 1000f;
+            this.position.X = 200;
+            this.position.Y = 300;
+        }
+
+
+
+        public override void Awake()
+        {
+
+        }
+
+        public override void Start()
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
 
     }
 }
--- Untitled Mormon game/Untitled Mormon game/Components/SpriteRenderer.cs
+++ Untitled Mormon game/Untitled Mormon game/Components/SpriteRenderer.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,24 @@ namespace Untitled_Mormon_game.Components
 {
     class SpriteRenderer : Component
     {
+        public Texture2D Sprite { get; set; }
 
+        public Vector2 Origin { get; set; }
+
+
+        public void SetSprite(string spriteName)
+        {
+            Sprite = GameWorld.Instance.Content.Load<Texture2D>(spriteName);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(Sprite, GameObject.Transform.Position, null, Color.White, 0, Origin, 1, SpriteEffects.None, 0);
+        }
+
+        public override string ToString()
+        {
+            return "SpriteRenderer";
+        }
     }
 }
